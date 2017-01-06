6e190656a912d29bf2d8b25e302b4c33e6aa5223
William Ganucheau <williamg.online@gmail.com>
2017-01-06 01:08:03 -0600

    Added 'Use item if possible' AI

--- game/Player.ts
+++ game/Player.ts
@@ -4,6 +4,7 @@
 
 import { Action, ActionType } from "./Action";
 import { GameState } from "./../game/Game";
+import { Vec2 } from "./Math"
 
 /**
  * There are two players per game
@@ -55,22 +56,67 @@ export abstract class Player {
 export class AIPlayer extends Player {
     private id: PlayerID | null;
     private action_cb: ActionCB | null;
+    private made_move: boolean;
 
     constructor() {
         super("AI Player");
 
         this.id = null;
         this.action_cb = null;
+        this.made_move = false;
     }
 
     init(id: PlayerID, state: GameState, action_cb: ActionCB): void {
         this.id = id;
         this.action_cb = action_cb;
+        this.made_move = false;
     }
 
     setState(state: GameState): void {
-        if (state.current_player == this.id) {
+        if (state.current_player != this.id) return;
+
+        /* If I've made my move, end my turn */
+        if (this.made_move) {
+            this.made_move = false;
             this.action_cb!(new Action(ActionType.END_TURN, null, null, null));
+            return;
         }
+
+        /* If it's my turn and I have an item, try to use it and then end my
+         * turn
+         */
+
+        /* Find a ship that's mine */
+        for (let [loc, ship] of state.grid.cells) {
+            if (ship == null) continue;
+            if (ship.player != this.id) continue;
+
+            for (let i  = 0; i < ship.items.length; ++i) {
+                const item = ship.items[i];
+
+                if (item == null) continue;
+
+                let desc = item.targetRequired();
+                let target: Vec2 | null = null;
+
+                if (desc != null) {
+                    for (let [loc, _] of state.grid.cells) {
+                        if (desc.matches(ship.id, loc, state)) {
+                            target = loc;
+                        }
+                    }
+                }
+
+
+                const action = new Action(ActionType.ACTIVATE, ship.id, i,
+                                          target);
+                this.made_move = true;
+                this.action_cb!(action);
+                return;
+            }
+        }
+
+        /* Couldn't find an item to use, just end turn */
+        this.action_cb!(new Action(ActionType.END_TURN, null, null, null));
     }
 }
